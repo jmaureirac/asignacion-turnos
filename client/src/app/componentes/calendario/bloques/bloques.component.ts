9dfd3aa7f2a61c64a41ae239eb5476321ca3117c
jmaureirac <jmaureira@icci.cl>
2018-01-24 01:15:45 -0300

    componentes calendario y bloques

---
+++ client/src/app/componentes/calendario/bloques/bloques.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bloques',
+  templateUrl: './bloques.component.html',
+  styleUrls: ['./bloques.component.css']
+})
+export class BloquesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
