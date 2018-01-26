ee6bae28c33bd58f7850fdcb715f56abeca41603
jmaureirac <jmaureira@icci.cl>
2018-01-25 23:08:30 -0300

    integrando calendario

--- client/src/app/componentes/calendario/calendario.component.ts
+++ client/src/app/componentes/calendario/calendario.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { Trabajador } from '../../modelos/db/trabajador';
+import { TrabajadorService } from '../../servicios/trabajador.service';
+import { Evento } from '../../modelos/db/evento';
+import { EventoService } from '../../servicios/evento.service';
+import { VARIABLES } from '../../modelos/variables';
 
 @Component({
   selector: 'app-calendario',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendario.component.css']
 })
 export class CalendarioComponent implements OnInit {
+  public dd:string;
+  public aa:string;
+  public yyyy:string;
 
-  constructor() { }
+  public current_d:any;
+  public current_m:any;
+  public current_y:any;
+
+
+  constructor() {
+  }
 
   ngOnInit() {
+    console.log(VARIABLES);
   }
 
 }
