59e01a6cc021be4f90c43aaf0e8e5b488483115f
jmaureirac <jmaureira@icci.cl>
2018-01-25 23:23:59 -0300

    path modelos fix

--- client/src/app/servicios/trabajador.service.ts
+++ client/src/app/servicios/trabajador.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { Trabajador } from '../modelos/trabajador';
+import { Trabajador } from '../modelos/db/trabajador';
 
 @Injectable()
 export class TrabajadorService{
