02b1cfee12026bc1235b4acb51bdd2ff6a944ba7
jmaureirac <jmaureira@icci.cl>
2018-01-26 02:34:02 -0300

    bloques comunicados con calendario y enviando dia+fecha

--- client/src/app/componentes/calendario/bloques/bloques.component.ts
+++ client/src/app/componentes/calendario/bloques/bloques.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { VARIABLES, Bloque } from '../../../modelos/variables';
 
 @Component({
   selector: 'app-bloques',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BloquesComponent implements OnInit {
 
-  constructor() { }
+  public head: string[];
+  public bloque: Bloque;
+  public rango_bloque: string[];
+  public date = new Date().getUTCDate();
+  public dias:string[];
 
-  ngOnInit() {
+  @Input() current_mm;
+  @Input() current_mes;
+  @Input() current_yyyy;
+  @Input() semana;
+
+  @Output() fechaClickeada = new EventEmitter<{fechaStringEvento:string}>();
+
+
+  constructor() {
+    this.head = VARIABLES.head;
+    this.rango_bloque = VARIABLES.rango_bloque;
+    this.dias = VARIABLES.dias;
   }
 
+  ngOnInit() {
+    //console.log(this.semana);
+  }
+
+  onClickDataCell(rango: string, data: string, dia_semana: number){
+    this.formatearFecha(rango ,data, dia_semana);
+  }
+
+  formatearFecha(rango: string, data: string, dia_semana: number){
+    let fechaFormateada = `${rango} del dia ${data} ${dia_semana} de ${this.current_mes} de ${this.current_yyyy} `;
+    this.emitirFecha(fechaFormateada);
+  }
+
+  emitirFecha(fechaString: string){
+    this.fechaClickeada.emit({fechaStringEvento: fechaString});
+  }
 }
